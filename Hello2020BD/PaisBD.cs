723d3322c5a06ee6d351018490088bc0bc8f007e
malom <malom@LAPTOP-7NPGQTEV>
2020-04-05 17:05:43 -0500

    Actualizar Clase Pais

---
+++ Hello2020BD/PaisBD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello2020BD
+{
+    public class PaisBD
+    {
+        DaneDivipolaEntities BD = new DaneDivipolaEntities();
+
+        private List<Pais> Pais_List()
+        {
+            return BD.Pais.ToList();
+        }
+
+        private void Pais_Inser(Pais pais)
+        {
+            BD.Pais.Add(pais);
+            BD.SaveChanges();
+        }
+
+
+    }
+}
