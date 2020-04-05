b20b54035fa8ab7676c5ead00cdeab11de07ce6d
malom <malom@LAPTOP-7NPGQTEV>
2020-04-05 17:56:47 -0500

    Actualización Aplicación WEB

---
+++ Hello2020WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hello2020WEB.MvcApplication" Language="C#" %>
---
+++ Hello2020WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hello2020WEB.Startup))]
+namespace Hello2020WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
