b556da4aadcd1a1cbb8668c359811b10862a3115
malom <malom@LAPTOP-7NPGQTEV>
2020-04-05 17:10:26 -0500

    Capa PRC Pais

---
+++ Hello2020PRC/PaisBD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hello2020BD;
+
+namespace Hello2020PRC
+{
+    public class PaisBD
+    {
+        PaisBD BD = new PaisBD();
+
+        public List<Pais> Pais_List()
+        {
+            return BD.Pais_List();
+        }
+    }
+}
